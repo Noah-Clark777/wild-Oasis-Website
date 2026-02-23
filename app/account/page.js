35da5617f327d21045d84ee8ec6582681a01d20c
Noah Clark <Clarknoah@live.com>
2026-02-23 17:57:09 -0600

    initial commit

---
+++ app/account/page.js
@@ -0,0 +1,9 @@
+export const metadata = { title: "Account" };
+
+export default function Page() {
+  return (
+    <h2 className="text-2xl font-semibold text-accent-400 mb-7">
+      Welcome, Noah
+    </h2>
+  );
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,40 @@
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
+import Filter from "@/app/_components/Filter";
+
+export const revalidate = 3600;
+// export const revalidate = 15;
+
+export const metadata = {
+  title: "Cabins",
+};
+
+export default async function Page({ searchParams }) {
+  const params = await searchParams;
+  const filter = params?.capacity ?? "all";
+
+  return (
+    <div>
+      <h1 className="mb-5 text-4xl font-medium text-accent-400">
+        Our Luxury Cabins
+      </h1>
+      <p className="mb-10 text-lg text-primary-200">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
+      </Suspense>
+    </div>
+  );
+}
