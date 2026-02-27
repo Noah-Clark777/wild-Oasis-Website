91afc8d88133f1ded0fcf394a9cc76ebb6eca575
Noah Clark <Clarknoah@live.com>
2026-02-27 13:31:53 -0600

    added user athentication and updated server client interaction

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,14 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = { title: "Account" };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  console.log(session);
+  const firstName = session.user.name.split(" ").at(0);
   return (
     <h2 className="text-2xl font-semibold text-accent-400 mb-7">
-      Welcome, Noah
+      Welcome, {firstName}
     </h2>
   );
 }
