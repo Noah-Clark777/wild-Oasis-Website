962104897b749e091f9c732f79a24d6b98534a90
Noah Clark <Clarknoah@live.com>
2026-03-02 16:34:36 -0600

    added delete funtion, and abuility to make reservations, finished project build

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,19 +1,22 @@
 "use client";
 
-import { useState } from "react";
+import { updateGuest } from "../_lib/actions";
+import SubmitButton from "./SubmitButton";
 
-function UpdateProfileForm({ children }) {
-  const [count, setCount] = useState();
-
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
+function UpdateProfileForm({ children, guest }) {
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
   return (
-    <form className="flex flex-col gap-6 px-12 py-8 text-lg bg-primary-900">
+    <form
+      action={updateGuest}
+      className="flex flex-col gap-6 px-12 py-8 text-lg bg-primary-900"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
+          name="fullName"
           className="w-full px-5 py-3 rounded-sm shadow-sm bg-primary-200 text-primary-800 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -22,6 +25,8 @@ function UpdateProfileForm({ children }) {
         <label>Email address</label>
         <input
           disabled
+          defaultValue={email}
+          name="email"
           className="w-full px-5 py-3 rounded-sm shadow-sm bg-primary-200 text-primary-800 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -43,13 +48,12 @@ function UpdateProfileForm({ children }) {
         <input
           name="nationalID"
           className="w-full px-5 py-3 rounded-sm shadow-sm bg-primary-200 text-primary-800"
+          defaultValue={nationalID}
         />
       </div>
 
       <div className="flex items-center justify-end gap-6">
-        <button className="px-8 py-4 font-semibold transition-all bg-accent-500 text-primary-800 hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <SubmitButton pendingLabel="updating...">Update Profile</SubmitButton>
       </div>
     </form>
   );
--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,10 +1,12 @@
-import ReservationCard from "@/app/_components/ReservationCard";
+import { getBookings } from "@/app/_lib/data-service";
+import { auth } from "@/app/_lib/auth";
+import ReservationList from "@/app/_components/ReservationList";
 
 export const metadata = { title: "Reservations" };
 
-export default function Page() {
-  // CHANGE
-  const bookings = [];
+export default async function Page() {
+  const session = await auth();
+  const bookings = await getBookings(session.user.guestId);
 
   return (
     <div>
@@ -20,11 +22,7 @@ export default function Page() {
           </a>
         </p>
       ) : (
-        <ul className="space-y-6">
-          {bookings.map((booking) => (
-            <ReservationCard booking={booking} key={booking.id} />
-          ))}
-        </ul>
+        <ReservationList bookings={bookings} />
       )}
     </div>
   );
